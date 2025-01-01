9a941e5d1eb3ceda0546f483c7282d5c50d90fa6
Cory Campbell <corycampbell@Corys-MacBook-Air.local>
2025-01-01 10:07:26 -0500

    Added font and text

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
 
-
 def main():
     pygame.init()
     screen = pygame.display.set_mode((1280, 720))
     pygame.display.set_caption('PONG')
+    pygame.font.init()
+    my_font = pygame.font.SysFont('Arial', 80, True)
     clock = pygame.time.Clock()
     running = True
     
@@ -26,7 +27,7 @@ def main():
     ball_vel = -10
     
     # Initial positions
-    player_x_pos = border
+    player_x_pos = 100
     player_y_pos = screen.get_height() / 2
     cpu_x_pos =screen.get_width() - border - paddle_width
     cpu_y_pos = screen.get_height() / 2
@@ -38,7 +39,12 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         
-        screen.fill("black")  
+        screen.fill("black") 
+        player_one_score_display = my_font.render(f'{player_one_score}', True, "white")
+        player_two_score_display = my_font.render(f'{player_two_score}', True, "white") 
+        screen.blit(player_one_score_display,(screen.get_width()/2 - 200, 50))
+        screen.blit(player_two_score_display,(screen.get_width()/2 + 200, 50))
+
         # border
         pygame.draw.rect(screen,"white",pygame.Rect(0,0,screen.get_width(),screen.get_height()), width=8)
         
@@ -62,7 +68,8 @@ def main():
         ball_x_pos += ball_vel
         
         # # Hamdle collision with paddle
-        if ball_x_pos <= player_x_pos + ball_width + paddle_width + 10:
+        # if ball_x_pos < player_x_pos + ball_width + paddle_width and ball_x_pos > player_x_pos - ball_width:
+        if ball_x_pos > player_x_pos:
             if ball_y_pos + ball_width >= player_y_pos and ball_y_pos - ball_width <= player_y_pos + paddle_height:
                 ball_vel *= -1
                 passes += 1
@@ -73,9 +80,21 @@ def main():
                 passes += 1
                 
         # Speed up
-        if passes == 10:
-            ball_vel *= 2
-            passes += 1
+        # if passes == 10:
+        #     ball_vel *= 2
+        #     passes += 1
+
+        # Round Win
+        if ball_x_pos > screen.get_width() - border:
+            player_one_score += 1
+            ball_vel = -10
+            ball_x_pos = screen.get_width() / 2
+            ball_y_pos = screen.get_height() / 2
+        elif ball_x_pos < border:
+            player_two_score += 1
+            ball_vel = -10
+            ball_x_pos = screen.get_width() / 2
+            ball_y_pos = screen.get_height() / 2
                 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
